c411122777b3d12d1167b7d5b0ec47b2165db349
rzazef-star <rzazef@gmail.com>
2026-03-27 20:12:24 +0100

    Update secure.js

--- cmds/secure.js
+++ cmds/secure.js
@@ -1,7 +1,30 @@
+const fs = require("fs");
+const config = require("../config.json");
+
 module.exports = {
     name: "secure",
-    whitelist: true,
-    execute(message) {
-        message.reply("Only Whitelist ! ");
+    async execute(message, args) {
+
+        if (!config.owners.includes(message.author.id))
+            return message.reply("Only Owner !");
+
+        if (!args[0]) 
+            return message.reply("Usage: +secure on / off");
+
+        const data = JSON.parse(fs.readFileSync("./secure.json"));
+
+        if (args[0] === "on") {
+            data.enabled = true;
+            fs.writeFileSync("./secure.json", JSON.stringify(data, null, 2));
+            return message.reply(" Secure ON");
+        }
+
+        if (args[0] === "off") {
+            data.enabled = false;
+            fs.writeFileSync("./secure.json", JSON.stringify(data, null, 2));
+            return message.reply(" Secure OFF");
+        }
+
+        message.reply("Usage: +secure on / off");
     }
 };
