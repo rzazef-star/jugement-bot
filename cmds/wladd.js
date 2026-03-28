eed44fcb6170264f60bc0e97cfa70ef5167f6f44
rzazef-star <rzazef@gmail.com>
2026-03-28 17:23:12 +0100

    Update wladd.js

--- cmds/wladd.js
+++ cmds/wladd.js
@@ -5,13 +5,14 @@ module.exports = {
     name: "wladd",
     execute(message, args) {
 
-        if (message.author.id !== config.owner && !whitelist.has(message.author.id))
-            return message.reply("wl only");
+        if (!config.owners.includes(message.author.id))
+            return message.channel.send("Owner only");
 
         const user = message.mentions.users.first();
-        if (!user) return message.reply("@user");
+        if (!user) return message.channel.send("Mention quelqu'un");
 
         whitelist.add(user.id);
-        message.reply(`${user.tag} add to wl`);
+
+        message.channel.send(`${user.tag} ajouté WL`);
     }
 };
